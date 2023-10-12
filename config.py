f25c9ec0d31ef4640c17f5cb85ff9ae163cf1b84
Alex <84103497+AngelGonzalez64@users.noreply.github.com>
2023-10-12 16:12:40 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import openai
 
-TOKEN = 'MTA5ODM3MTAyNjUxNTE0NDg0NQ.GzpRbJ.N-6Tq229hNwXO-Mj4D7KSReN2Y3V3wCBTnZ5To'# Token de Discord
+TOKEN = ''# Token de Discord
 COMMAND_PREFIX = "!"
 
 # API de OpenAI
-openai.api_key = "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" # TU_API_KEY_AQUÍ
+openai.api_key = "" # TU_API_KEY_AQUÍ
 
 #Permisos (se pone como custom link en el bot OAuth2)
 #https://discord.com/developers/applications
